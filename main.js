b8af41cb3261b7b1b76d75babdc52a8ffbaf1d40
Jordan Dalton <jordan.8474@gmail.com>
2015-02-12 11:47:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-var jqXHR = $.get( "artists.json", function ( data){
-	alert( data );
-}, "json");
+var jqXHR = $.ajax({
+	url: "artists.json",
+	dataType: "test",
+	success: function(data){
+		var json = $.parseJSON(data);
+		alert (json.toString());
+	}
+})
 
 //jQuery.get( url [, data ] [, success ] [, dataType ] ) 
+/*
+ $.ajax({
+                    url: "data.json",
+                    //force to handle it as text
+                    dataType: "text",
+                    success: function(data) {
+                        
+                        //data downloaded so we call parseJSON function 
+                        //and pass downloaded data
+                        var json = $.parseJSON(data);
+                        //now json variable contains data in json format
+                        //let's display a few items
+                        $('#results').html('Plugin name: ' + json.name + '<br />Author: ' + json.author.name);
+                    }
+                });
+*/
